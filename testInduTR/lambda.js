d3987bef61d4c8455ca745aab2d0cc046f84bd92
Indunil1992 <indunil@adroitlogic.com>
2019-09-11 16:05:59 +0530

    a

--- testInduTR/lambda.js
+++ testInduTR/lambda.js
@@ -2,23 +2,20 @@ let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = function (event, context, callback) {
-    ddb.get({
-        TableName: 'table2',
-        Key: {
-            '3': '3',
-            '4': '4'
-        }
+    ddb.query({
+        TableName: 'sample1'
     }).promise()
         .then((data) => {
-            console.log("dataaaa");
-            console.log(data);
+             console.log("dataaa");
+              console.log(data);
             //your logic goes here
         })
         .catch((err) => {
-            console.log("errrr");
-            console.log(err);
+             console.log("errrr");
+              console.log(err);
             //handle error
         });
+
 
     callback(null, { "message": "Successfully executed" });
 }
